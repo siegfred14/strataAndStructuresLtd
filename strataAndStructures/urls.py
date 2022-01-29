cdeeaaf8251de7b39a491791eb22421f5058cdb3
Siegfred Samson <siegfredsamson@gmail.com>
2022-01-29 12:27:55 +0100

    include imported in project urls.py

--- strataAndStructures/urls.py
+++ strataAndStructures/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.urls.conf import include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.static import serve
@@ -23,7 +24,6 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('coreApp.urls')),
-
     url(r'^media/(?P<path>.*)$', serve,
         {'document_root':       settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,
