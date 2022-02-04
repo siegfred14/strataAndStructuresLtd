c753ece95581483c98d9b9ad5af76ebd53f6de73
Siegfred Samson <siegfredsamson@gmail.com>
2022-02-04 20:19:04 +0100

    services endpoint refactored

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,15 @@
 
+// page loader js
+
+$(document).ready(function () {
+    'use strict';
+
+    //********* page loader js
+
+    setTimeout(function () {
+        $('.loader_bg').fadeToggle();
+    }, 1500);
+})
+
 let footer_date = document.querySelector(".footer-date");
 footer_date.document.write(new Date().getFullYear());
