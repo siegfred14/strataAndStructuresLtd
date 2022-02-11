af8cb8c080fd686ffc3ea036cc9cbbaa49a3d88c
Siegfred Samson <siegfredsamson@gmail.com>
2022-02-11 09:56:21 +0100

    deleting redundant code

--- static/js/index.js
+++ static/js/index.js
@@ -10,14 +10,3 @@ $(document).ready(function () {
         $('.loader_bg').fadeToggle();
     }, 1500);
 })
-
-// let footer_date = document.querySelector(".footer-date");
-// footer_date.addEventListener("load", function name(params) {
-//     // document.write(new Date().getFullYear());
-//     let currentYear = new Date().getFullYear();
-//     footer_date.innerHTML = currentYear;
-// })
-
-// Footer
-let date = new Date().getFullYear();
-document.querySelector(".footer-year").innerHTML = date;
